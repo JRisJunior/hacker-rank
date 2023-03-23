ed20b6546fe41abab2b6097e33e8b5d96fb0f2dc
JRisJunior <jeffmrubin@gmail.com>
2023-03-23 12:17:41 -0500

    resolve each loop bug

--- mini-max-sum.rb
+++ mini-max-sum.rb
@@ -1,17 +1,20 @@
-
+arr = [1, 2, 3, 4, 5]
 
 def miniMaxSum(arr)
   # Write your code here
   leave_out = 0
   total_sum = 0
   four_array = []
-  arr.each do |index|
-    total_sum = total_sum + arr[index]
+  arr.each do |value|
+    p value
+    total_sum += value
+    p total_sum
   end
   while leave_out < arr.length
     four_array << total_sum - (arr[leave_out])
     leave_out += 1
   end
-  
-
+  p four_array
 end
+
+miniMaxSum(arr)
